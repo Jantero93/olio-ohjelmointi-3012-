1a416f6295366b3dc52b8a81a95135778006e2d2
Jani Sillanpää <jani.k.sillanpaa@tuni.fi>
2019-09-04 13:57:34 +0300

    Teht 3 yms

--- vko36/Teht2/Source.cpp
+++ vko36/Teht2/Source.cpp
@@ -2,11 +2,11 @@
 using std::cout; using std::endl;
 
 int main() {
-	double taulukko[]{ 7.8, 8.1, 8.05, 7.7, 7.9, 8.21 };
+	double kulutustaulukko[]{ 7.8, 8.1, 8.05, 7.7, 7.9, 8.21 };
 
 	cout << "Normikulutuslukemat (l/100) ovat:" << endl;
 	for (int i = 0; i < 6; i++) {
-		cout << i+1 << ". Lukema: " << taulukko[i] << endl;
+		cout << i+1 << ". Lukema: " << kulutustaulukko[i] << endl;
 	}
 
 	system("pause");
