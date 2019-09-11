bbf60295f90a267e019ff636b9c0ea7f210827a2
Jani Sillanpää <jani.k.sillanpaa@tuni.fi>
2019-09-11 18:19:56 +0300

    teht1-2

--- tehtava1/vko37/mainPaivays.cpp
+++ tehtava1/vko37/mainPaivays.cpp
@@ -4,7 +4,8 @@
 int main() {
 	Paivays tanaan;
 	Paivays huomenna;
-
+	
+	//tänään
 	tanaan.asetaPaiva(11);
 	tanaan.asetaKuukausi(9);
 	tanaan.asetaVuosi(2019);
@@ -12,9 +13,12 @@ int main() {
 	tanaan.tulostaPaivays();
 
 	//huomenna
-	huomenna.asetaPaiva();
+	huomenna.asetaPaiva(12);
+	huomenna.asetaKuukausi(9);
+	huomenna.asetaVuosi(2019);
 
-	std::cout << tanaan.annaKuukausi() << "/" << tanaan.annaPaiva() << "/" << tanaan.annaVuosi() << std::endl;
+	std::cout << "Tanaan on paiva: "  << tanaan.annaKuukausi() << "/" << tanaan.annaPaiva() << "/" << tanaan.annaVuosi() << std::endl;
+	std::cout << "Huomenna on paiva: " << huomenna.annaKuukausi() << "/" << huomenna.annaPaiva() << "/" << huomenna.annaVuosi() << std::endl;
 
 	system("pause");
 	return 0;
