8b611c25c64648bd13d5fd800b97195d81386bfd
Jani Sillanpää <jani.k.sillanpaa@tuni.fi>
2019-09-11 17:57:12 +0300

    test123

--- tehtava1/vko37/mainPaivays.cpp
+++ tehtava1/vko37/mainPaivays.cpp
@@ -3,12 +3,16 @@
 
 int main() {
 	Paivays tanaan;
+	Paivays huomenna;
 
 	tanaan.asetaPaiva(11);
 	tanaan.asetaKuukausi(9);
 	tanaan.asetaVuosi(2019);
 
 	tanaan.tulostaPaivays();
+
+	//huomenna
+	huomenna.asetaPaiva();
 
 	std::cout << tanaan.annaKuukausi() << "/" << tanaan.annaPaiva() << "/" << tanaan.annaVuosi() << std::endl;
 
