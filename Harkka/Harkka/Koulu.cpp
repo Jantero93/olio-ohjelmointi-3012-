01508bce41fe5e93b6a9c1e43839f9bd2a1ed6b1
Jani Sillanpää <jani.k.sillanpaa@tuni.fi>
2019-10-09 20:57:31 +0300

    teht1-4 tehty harkka solutioniin

---
+++ Harkka/Harkka/Koulu.cpp
@@ -0,0 +1,95 @@
+#include "Koulu.h"
+#include <iostream>
+using std::cout; using std::cin; using std::endl;
+
+
+Koulu::Koulu():nimi_()
+{
+}
+
+Koulu::Koulu(const string& nimi):nimi_(nimi)
+{
+}
+
+
+Koulu::~Koulu()
+{
+}
+
+void Koulu::lisaaKoulutusOhjelma()
+{
+	//toimii
+	cout << "Anna koulutusohjelman nimi: ";
+	string koulutusohjelma;
+	getline(cin, koulutusohjelma);
+	koulutusohjelmat_.push_back(koulutusohjelma);
+}
+
+string Koulu::annaNimi() const
+{
+	return nimi_;
+}
+
+void Koulu::asetaNimi(const string & nimi)
+{
+	nimi_ = nimi;
+}
+
+void Koulu::tulostaKoulutusOhjelmat() const
+{
+	//toimii
+	string temp;
+
+	for (unsigned int i = 0; i < koulutusohjelmat_.size(); i++)
+	{
+		temp = koulutusohjelmat_[i].annaNimi();
+		cout << temp << " ";
+	}
+}
+
+void Koulu::tulostaKoulutusOhjelmienMaara() const
+{
+	//toimii
+	cout << koulutusohjelmat_.size();
+}
+
+void Koulu::lisaaKoulutusOhjelmaanOpettaja()
+{
+	//toimii
+	cout << "Anna koulutusohjelman nimi: ";
+	string koulutusohjelma;
+	getline(cin, koulutusohjelma);
+	koulutusohjelmat_.push_back(koulutusohjelma);
+	
+	Koulutusohjelma temp;
+	temp.lisaaOpettaja();
+}
+
+void Koulu::tulostaKoulutusOhjelmanOpettajat() const
+{
+
+	//toimii
+	for (unsigned int i = 0; i < koulutusohjelmat_.size(); i++) {
+		koulutusohjelmat_[i].tulostaOpettajat();
+	}
+}
+
+void Koulu::lisaaKoulutusOhjelmaanOpiskelija()
+{
+	//toimii
+	cout << "Anna koulutusohjelman nimi: ";
+	string koulutusohjelma;
+	getline(cin, koulutusohjelma);
+	koulutusohjelmat_.push_back(koulutusohjelma);
+	
+	Koulutusohjelma temp;
+	temp.lisaaOpiskelija();
+}
+
+void Koulu::tulostaKoulutusOhjelmanOpiskelijat() const
+{
+	//toimii
+	for (unsigned int i = 0; i < koulutusohjelmat_.size(); i++) {
+		koulutusohjelmat_[i].tulostaOpiskelija();
+	}
+}
--- Harkka/Harkka/main.cpp
+++ Harkka/Harkka/main.cpp
@@ -1,19 +1,38 @@
-#include "Koulutusohjelma.h";
+//#include "Koulutusohjelma.h";
+#include "Koulu.h"
+#include <iostream>
+
 
 
 int main() {
 
-	Koulutusohjelma tite("TiTe");
-	tite.lisaaOpettaja();
-	tite.lisaaOpettaja();
-	tite.lisaaOpiskelija();
-	tite.lisaaOpiskelija();
+	using std::cout;
+	using std::endl;
+	using std::endl;
 
-	tite.tulostaOpettajat();
-	tite.tulostaOpiskelija(); 
+	Koulutusohjelma tite("TiTe");
+	Koulutusohjelma raksa("Rakennus");
+	//tite.lisaaOpettaja();
+	//tite.lisaaOpettaja();
+	//tite.lisaaOpiskelija();
+	//tite.lisaaOpiskelija();
+
+	//tite.tulostaOpettajat();
+	//tite.tulostaOpiskelija(); 
+
+
+	Koulu tamk;
+	
+	tamk.lisaaKoulutusOhjelmaanOpiskelija();
+	tamk.lisaaKoulutusOhjelmaanOpiskelija();
+	tamk.lisaaKoulutusOhjelmaanOpiskelija();
+
+	tamk.tulostaKoulutusOhjelmat();
+
+	tamk.tulostaKoulutusOhjelmienMaara();
 
 	
 
-		system("pause");
+	system("pause");
 	return 0;
 }
